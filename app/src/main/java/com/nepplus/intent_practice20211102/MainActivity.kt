75d98c210179a01dcb4e41e9fa344ece02a88fc7
이아현 <suk03029@gmail.com>
2021-11-03 09:14:06 +0900

    뉴 인텐트 변수 선언 및 넘어가기 메소드 사용

--- app/src/main/java/com/nepplus/intent_practice20211102/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_practice20211102/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.nepplus.intent_practice20211102
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoIntense.setOnClickListener {
+
+            val newIntent = Intent(this,Practiceintence::class.java)
+            startActivity(newIntent)
+//  다른 화면으로 넘어가는 변수 선언 및 명령문 연습
+        }
     }
 }
