6470fce3f672941c508220ec62c38d21883246b5
이아현 <suk03029@gmail.com>
2021-11-03 09:16:34 +0900

    새로운 화면에서 finish 사용하여 돌아가기

--- app/src/main/java/com/nepplus/intent_practice20211102/Practiceintence.kt
+++ app/src/main/java/com/nepplus/intent_practice20211102/Practiceintence.kt
@@ -2,10 +2,16 @@ package com.nepplus.intent_practice20211102
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_practiceintence.*
 
 class Practiceintence : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_practiceintence)
+
+        btnGoMain.setOnClickListener {
+
+            finish()
+        }
     }
 }
